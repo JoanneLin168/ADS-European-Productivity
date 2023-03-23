3d4f08a9a7a6426c989e4796d9dcb3508bd1ac52
JoanneLin168 <joannelin168@outlook.com>
2023-03-23 10:41:29 +0000

    Updated webscraper to include Europe subregions

--- Libraries/countries.py
+++ Libraries/countries.py
@@ -1,8 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
-print("This may take some time...")
+############################################################################
+def convert_iso_2_to_3(iso_2):
+    return iso_2_to_3_map[iso_2]
 
+def convert_iso_3_to_2(iso_3):
+    return iso_3_to_2_map[iso_3]
+
+def get_iso_2_list():
+    return list(iso_2_to_3_map.keys())
+
+def get_iso_3_list():
+    return list(iso_3_to_2_map.keys())
+
+############################################################################
+# Get ISO-2 and ISO-3 data
+print("Getting ISO-2 and ISO-3 data...")
 url = "https://www23.statcan.gc.ca/imdb/p3VD.pl?Function=getVD&TVD=141329"
 req = requests.get(url)
 source = req.text
@@ -23,14 +37,26 @@ for i in range(N):
     iso_2_to_3_map[iso_2_elmts[i].text] = iso_3_elmts[i].text
     iso_3_to_2_map[iso_3_elmts[i].text] = iso_2_elmts[i].text
 
-def convert_iso_2_to_3(iso_2):
-    return iso_2_to_3_map[iso_2]
 
-def convert_iso_3_to_2(iso_3):
-    return iso_3_to_2_map[iso_3]
+# Get Europe subregions
+print("Getting Europe subregions...")
+url = "https://www.worldometers.info/population/europe/"
+req = requests.get(url)
+source = req.text
+soup = BeautifulSoup(source, 'html.parser')
 
-def get_iso_2_list():
-    return list(iso_2_to_3_map.keys())
+subregions_columns = soup.find_all('div', attrs={
+    'class': 'col-md-6 noli'
+})
 
-def get_iso_3_list():
-    return list(iso_3_to_2_map.keys())
+subregions = {}
+for col in subregions_columns:
+    elmts = col.find_all('a')
+    subregion_elmt = elmts[0]
+    countries_elmt = elmts[1:]
+    
+    subregion = subregion_elmt.text
+    subregions[subregion] = []
+    for country_elmt in countries_elmt:
+        country = country_elmt.text
+        subregions[subregion].append(country)
